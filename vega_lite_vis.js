a685390cb393903355e99557aab8e579388c672f
Hao Yang <hlee0163@student.monash.edu>
2025-10-10 00:50:20 +0800

    added new graph to website

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,7 +1,12 @@
 var vg_1 = "stations.vg.json";
+var vg_2 = "stripplot.vg.json";
 
 vegaEmbed("#stations", vg_1).then(function(result) {
 
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+
+}).catch(console.error);
+
+vegaEmbed("#stripplot", vg_2).then(function(result) {
 
 }).catch(console.error);
